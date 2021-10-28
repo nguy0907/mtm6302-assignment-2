755f714f2fe98cda4237007b1c7b701c3d8ccc5e
nguy0907 <nguy0907@algonquinlive.com>
2021-10-28 03:08:12 -0400

    adding js and css

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,6 +1,6 @@
-const $container = document.getElementById('container')
 const $title = document.getElementById('title')
 const $name = document.getElementById('name')
+const $container = document.getElementById('container')
 const $imageContainer = document.getElementById('imageContainer')
 const $picture = document.getElementById('picture')
 const $picText = document.getElementById('picText')
@@ -10,12 +10,16 @@ $title.textContent = 'Image Gallery'
 $name.textContent = 'Richard N'
 
 
+
 $picture.addEventListener('click', function (event){
-$picText.textContent = 'This is an image'
+$picText.innerHTML = 'This is an image'
 })
 
 for (let picNumber = 0; picNumber < 12; picNumber++) {
-$container.innerHTML += `<div id='container'>
-                        <img id='picture' src="https://picsum.photos/300/300?random=1">
-                        <p id='picText'></p> </div>`
+    $container.innerHTML += `<div id='imageContainer'>
+    <img id='picture' src="https://picsum.photos/300/300?random=1">
+    <p id='picText'></p> </div>`
+
+
 }
+$picText.innerText = 'Click Me'
