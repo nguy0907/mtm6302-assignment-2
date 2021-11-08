2f4e04d901f42ab9f6c2bd1fa13194421e9fe6a6
nguy0907 <nguy0907@algonquinlive.com>
2021-11-08 10:07:59 -0500

    adding js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const $title = document.getElementById('title')
 const $name = document.getElementById('name')
 const $container = document.getElementById('container')
 const $imageContainer = document.getElementById('imageContainer')
-const $picture = document.getElementById('picture')
 const $picText = document.getElementById('picText')
 const $element = document.getElementById('element')
+const $picture = document.getElementById('picture')
+const $popuptext = document.getElementById('popuptext')
+
+// const $allPictures = document.querySelector('picture')
+
 
 $title.textContent = 'Image Gallery'
 $name.textContent = 'Richard N'
@@ -25,33 +29,33 @@ const imgArray = [
 ]
 
 const img = [
-    `<img src="https://picsum.photos/300/300?random=1" ${imgArray[0]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[1]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[2]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[3]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[4]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[5]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[6]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[7]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[8]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[9]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[10]}>
-    <img src="https://picsum.photos/300/300?random=1" ${imgArray[11]}>`
+    `<img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[0]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[1]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[2]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[3]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[4]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[5]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[6]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[7]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[8]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[9]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[10]}>
+    <img id='picture' src="https://picsum.photos/300/300?random=1" ${imgArray[11]}>`
 ]
-$container.innerHTML = img
-$picture.addEventListener('click', function (){
-    img.innerHTML = 'hi'
+$imageContainer.innerHTML = img
+$imageContainer.addEventListener('click', function (){
+    $popuptext.textContent = 'hi'
     console.log(img)
 })
 
 
 
-// for (let picNumber = 0; picNumber < 11; picNumber++) {
-//     $imageContainer.innerHTML += `<div id='imageContainer'>
-//     <img id='picture' src="https://picsum.photos/300/300?random=1">
-//     </div>`
+for (let picNumber = 0; picNumber < 11; picNumber++) {
+    // $imageContainer.innerHTML += `<div id='imageContainer'>
+    // <img id='picture' src="https://picsum.photos/300/300?random=1">
+    // </div>`
 
    
-// }
+}
 
 
